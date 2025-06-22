7209ddabca8b74f8fa361f18253827675e796544
Kristine Gallardo <kristine.gallardo.dev@gmail.com>
2025-06-22 12:32:08 +0800

    Updated schema migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_06_21_122050) do
+ActiveRecord::Schema[8.0].define(version: 2025_06_21_163740) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "order_items", force: :cascade do |t|
+    t.bigint "order_id", null: false
+    t.bigint "product_id", null: false
+    t.integer "quantity", default: 1, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["order_id"], name: "index_order_items_on_order_id"
+    t.index ["product_id"], name: "index_order_items_on_product_id"
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.string "reference_number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "customer_name"
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -21,4 +38,18 @@ ActiveRecord::Schema[8.0].define(version: 2025_06_21_122050) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "promotions", force: :cascade do |t|
+    t.bigint "product_id", null: false
+    t.string "code"
+    t.string "name"
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_promotions_on_product_id"
+  end
+
+  add_foreign_key "order_items", "orders"
+  add_foreign_key "order_items", "products"
+  add_foreign_key "promotions", "products"
 end
