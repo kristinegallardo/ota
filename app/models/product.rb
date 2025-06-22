46d8026d60374f4ba00b5353345fad8444d73e04
Kristine Gallardo <kristine.gallardo.dev@gmail.com>
2025-06-23 02:10:16 +0800

    Added table relationship and constant promotion_types

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,8 @@
 class Product < ApplicationRecord
-  has_one :promotion
+  belongs_to :promotion, optional: true
+
   has_many :order_items
   has_many :orders, through: :order_items
+
+  validates :code, uniqueness: true
 end
--- app/models/promotion.rb
+++ app/models/promotion.rb
@@ -1,4 +1,8 @@
 class Promotion < ApplicationRecord
-  belongs_to :product
+  PROMOTION_TYPES = %w[fixed bundle bulk percentage].freeze
 
+  has_many :products, inverse_of: :promotion, dependent: :destroy
+  accepts_nested_attributes_for :products, allow_destroy: true, reject_if: :all_blank
+
+  validates :promotion_type, inclusion: { in: PROMOTION_TYPES, allow_blank: false }
 end
